6cb76364151134dbd5c6bd15082e4d0904a12a3a
pawelandruszkow <andruszkowpawel@gmail.com>
2021-05-02 14:58:41 +0100

    Bare-bones for step 2, imported LinkedList and SortedADT library code

---
+++ src/SortedADT.java
@@ -0,0 +1,17 @@
+
+public abstract interface SortedADT {
+
+    public class NotFoundException extends Exception {
+    }
+
+    public class NotUniqueException extends Exception {
+    }
+
+    public abstract void insert(Comparable object) throws NotUniqueException;
+
+    // returns the object found
+    public abstract Comparable remove(Comparable object) throws NotFoundException;
+
+    // returns the object removed
+    public abstract Comparable find(Comparable object) throws NotFoundException;
+}
